ff4121e7783842bb305ba1efb94a1edc4e404271
Kyle Samuel <Ksamuel92@gmail.com>
2021-03-22 10:55:15 -0400

    commit tree 7525e7427402e48ad6586aec98e7a504528e7b81

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
   #your code here
+  attr_accessor
+def initialize (attribute_hash)
+    attribute_hash.each do |key, value| 
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
+end
 end
